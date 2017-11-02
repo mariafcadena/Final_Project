4950fcf6da76a4ea6b432cff3e8c9af6da664753
Maria F Cadena <mariafcadena@Marias-MBP.fritz.box>
2017-11-02 14:49:25 +0100

    Creation of functions  - FixCaract  - CleanPieces  - CleanChambres  - CleanArea Change of name to the function  - FixPrice

--- Functions.R
+++ Functions.R
@@ -10,6 +10,7 @@ CleanProperties <- function(Vector)
   return(NewVec)
 }
 
+#IDENTIFIES THE NEIGHBOURHOOD OF PROPERTY IN EACH ELEMENT OF THE VECTOR
 CleanCartier <- function(Vector)
 {
   NewVec<-lapply(Vector,function(x){stri_trans_general(x,"Latin-ASCII")})
@@ -17,7 +18,7 @@ CleanCartier <- function(Vector)
 }
 
 #FIXES THE PRICE ON ONE ELEMENT
-FixedPrice <- function(Price)
+FixPrice <- function(Price)
 {
   P1<-stri_trans_general(trimws(gsub("^\\s*<U\\+\\w+>|-", " ", Price)),"Latin-ASCII")
   P2<-regmatches(P1,gregexpr(" ", P1),TRUE)
@@ -29,9 +30,58 @@ FixedPrice <- function(Price)
   return(as.numeric(FinalPrice))
 }
 
-#CLEANS DE PRICE ON EVERY ITEM OF THE VECTOR
+#CLEANS THE PRICE ON EVERY ITEM OF THE VECTOR
 CleanPrice <- function(Vector)
 {
   NewVec<-lapply(Vector,FixedPrice)
+  return(NewVec)
+}
+
+#FIXES THE VALUES OF ALL THE CHARACTERISTICS OF THE PROPERTY
+FixCaract <- function(Caract, Type)
+{
+  P1  <-  stri_trans_general(trimws(gsub("^\\s*<U\\+\\w+>|-", " ", Caract)),"Latin-ASCII")
+  if (Type=="piece")
+  {
+    P2  <-  regmatches(P1,gregexpr("p", P1),TRUE)
+    NewCaract <- trimws(P2[[1L]][1], "both")
+  }
+  if (Type=="chambre")
+  {
+    P2  <-  regmatches(P1,gregexpr("p", P1),TRUE)
+    P3  <-  regmatches(P2[[1L]][2],gregexpr("chb", P2[[1L]][2]),TRUE)
+    if (length(P3[[1L]])==1){NewCaract<-0}
+    else {NewCaract<-stri_trans_general(trimws(gsub("^\\s*<U\\+\\w+>|-", " ", P3[[1L]][1])),"Latin-ASCII")}
+  }
+  if (Type=="area")
+  {
+    P2  <-  regmatches(P1,gregexpr("chb", P1),TRUE)
+    if (length(P2[[1L]])==1){P2  <-  regmatches(P1,gregexpr("p", P1),TRUE)}
+    P3  <-  regmatches(P2[[1L]][2],gregexpr("m", P2[[1L]][2]),TRUE)
+    NewCaract<-stri_trans_general(trimws(gsub("^\\s*<U\\+\\w+>|-", " ", P3[[1L]][1])),"Latin-ASCII")
+  }
+  if (grepl(",",NewCaract)==TRUE){NewCaract<-gsub(",", ".", NewCaract)}
+  options(digits=5)
+  return(as.numeric(NewCaract))
+}
+
+#CLEANS THE PIECES ON EVERY ITEM OF THE VECTOR
+CleanPiece <- function(Vector)
+{
+  NewVec<-lapply(Vector, FixCaract, Type="piece")
+  return(NewVec)
+}
+
+#CLEANS THE NUMBER OF ROOMS ON EVERY ITEM OF THE VECTOR
+CleanChambre <- function(Vector)
+{
+  NewVec<-lapply(Vector, FixCaract, Type="chambre")
+  return(NewVec)
+}
+
+#CLEANS THE AREA ON EVERY ITEM OF THE VECTOR
+CleanArea <- function(Vector)
+{
+  NewVec<-lapply(Vector, FixCaract, Type="area")
   return(NewVec)
 }
